b2aab346538d23fe4441b226a435bd77617899b0
tgsegata <tgsegata@gmail.com>
2025-07-16 01:13:23 -0400

    I added three library in another html & js

---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 100);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({
+    color: 0x00ff00
+});
+const cube = new THREE.Mesh(geometry, material);
+cube.scale.setScalar(2);
+scene.add(cube);
+
+const hemilight = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+scene.add(hemilight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
+
