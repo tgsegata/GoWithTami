5e926c91a6e42a08041eb4efc2f3f45803fd7faa
tgsegata <tgsegata@gmail.com>
2025-07-16 00:45:56 -0400

    im lost

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  anime({
-    targets: '.cta',
-    translateY: [20, 0],
-    opacity: [0, 1],
-    delay: anime.stagger(300),
-    duration: 1000,
-    easing: 'easeOutExpo'
-  });
+import { animate, utils, createDraggable, createSpring } from 'animejs';
+
+const [ $logo ] = utils.$('.logo.js');
+const [ $button ] = utils.$('button');
+let rotations = 0;
+
+// Created a bounce animation loop
+animate('.logo.js', {
+  scale: [
+    { to: 1.25, ease: 'inOut(3)', duration: 200 },
+    { to: 1, ease: createSpring({ stiffness: 300 }) }
+  ],
+  loop: true,
+  loopDelay: 250,
 });
+
+// Make the logo draggable around its center
+createDraggable('.logo.js', {
+  container: [0, 0, 0, 0],
+  releaseEase: createSpring({ stiffness: 200 })
+});
+
+// Animate logo rotation on click
+const rotateLogo = () => {
+  rotations++;
+  $button.innerText = `rotations: ${rotations}`;
+  animate($logo, {
+    rotate: rotations * 360,
+    ease: 'out(4)',
+    duration: 1500,
+  });
+}
+
+$button.addEventListener('click', rotateLogo);
